58abd492df28fda9fc0cd89b54c7312658595c24
magnuswmiller <mgnsmiller@gmail.com>
2024-11-12 17:56:30 -0700

    Portfolio.py created.

--- Portfolio.py
+++ Portfolio.py
@@ -1,1 +1,17 @@
-print("Hello world")
+# Portfolio.py
+# Handles CSV importing and handling
+
+#Library Import:
+import pandas as pd
+
+def main():
+    filePath = input("* Please enter file path for portfolio data: ")
+    print("* Loading data from file...")
+    rawPortfolio = pd.read_csv(filePath, skiprows=1)
+    rawPortfolio.columns = ['Ticker',
+                            'SettlementDate',
+                            'Principal']
+    print(rawPortfolio)
+
+if __name__ == '__main__':
+    main()
