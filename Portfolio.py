e61028e89fd08ae51a059264e7a9121915f7566e
magnuswmiller <mgnsmiller@gmail.com>
2025-02-19 15:15:09 -0700

    parser is working, beginning handling stock data.

--- Portfolio.py
+++ Portfolio.py
@@ -4,6 +4,7 @@
 #Library Import:
 import pandas as pd
 import numpy as np
+import yfinance as yf
 
 class Stock():
     def __init__(self, ticker, amnt, cost, sd, sector):
@@ -12,6 +13,14 @@ class Stock():
         self.cost = float(cost)
         self.sd = sd
         self.sector = sector
+        self.yfTicker = yf.Ticker(self.ticker)
+        self.history = self.yfTicker.history()
+        #self.currentPrice = self.yfTicker.info['currentPrice']
+        #print(type(self.history['Close'].tail(1).to_list()))
+        #print(self.history['Close'].tail(1).to_list())
+        self.currentPrice = self.history['Close'].tail(1).to_list()
+        print(self.currentPrice[0])
+
 
     def getTicker(self):
         return self.ticker
@@ -29,14 +38,11 @@ class Stock():
         return self.sector
 
     def setAmnt(self, newAmnt):
-        self.amnt += newAmnt
+        self.amnt = newAmnt
         return self.amnt
 
-    def setCost(self, newCost, isBuy):
-        if isBuy:
-            self.cost += newCost
-        else:
-            self.cost -= newCost
+    def setCost(self, newCost):
+        self.cost = newCost
         return self.cost
 
     def setSD(self, newSD):
@@ -70,8 +76,11 @@ class Portfolio():
 
     def getPositions(self):
         return self.positions
+
+    def getCash(self):
+        return self.cash
     
-    def getLenth(self):
+    def getLength(self):
         return len(self.positions)
 
     def getSD(self):
@@ -104,14 +113,47 @@ class Portfolio():
     def addPosition(self, key, value, stock):
         self.log.update({key: value})
         self.positions.append(stock)
-
+        self.cash -= stock.getCost()
         return -1
 
-    def updatePosition(self, ticker, amnt, cost, sd):
+    def updatePosition(self, ticker, amnt, cost, sd, isBuy):
+        oldAmnt = self.log.get(ticker)
+        oldCost = -1
+        index = -1
+        for i in range(len(self.positions)):
+            if(self.positions[i].getTicker() == ticker):
+                index = i
+                oldCost = self.positions[i].getCost()
+                break
+        if isBuy:
+            self.log.update({ticker: oldAmnt + amnt})
+            self.positions[index].setAmnt(oldAmnt + amnt)
+            self.positions[index].setCost(oldCost + cost)
+            self.positions[index].setSD(sd)
+            self.cash -= amnt
+        else:
+            if(oldAmnt - amnt) <= 0:
+                self.exitPosition(ticker)
+            else:
+                self.log.update({ticker: oldAmnt - amnt})
+                self.positions[index].setAmnt(oldAmnt - amnt)
+                self.positions[index].setCost(oldCost - cost)
+                self.positions[index].setSD(sd)
+                self.cash += amnt
         return -1
 
     #TODO
-    def exitPosition(self, target):
+    def exitPosition(self, ticker):
+        self.log.pop(ticker)
+        oldCost = -1
+        index = -1
+        for i in range(len(self.positions)):
+            if(self.positions[i].getTicker() == ticker):
+                index = i
+                oldCost = self.positions[i].getCost()
+                break
+        self.cash += oldCost
+        self.positions.pop(index)
         return -1
 
     #TODO: Finish to string method
@@ -127,12 +169,17 @@ def portfolioParse(rawPortfolioArr, portfolio):
         amnt = rawPortfolioArr[i][1]
         costPS = rawPortfolioArr[i][2]
         sd = rawPortfolioArr[i][3]
-        sector = rawPortfolioArr[i][4]
+        sector = rawPortfolioArr[i][5]
+        isBuy = -1
+        if(rawPortfolioArr[i][4] == 'BUY'):
+            isBuy = True
+        else:
+            isBuy = False
         if not(portfolio.inLog(ticker)):
             info = rawPortfolioArr[i][1:]
             portfolio.addPosition(ticker, amnt, Stock(ticker, amnt, costPS*amnt, sd, sector))
         else:
-            portfolio.updatePosition(ticker, amnt, costPS, sd)
+            portfolio.updatePosition(ticker, amnt, costPS, sd, isBuy)
     return -1 
 
 def main():
@@ -150,21 +197,7 @@ def main():
     currentPortfolio = Portfolio()
     portfolioParse(rawPortfolioArr, currentPortfolio)
     print(currentPortfolio)
+    print(currentPortfolio.getCash())
 
 if __name__ == '__main__':
     main()
-
-'''
-filePath = input("* Please enter file path for portfolio data: ")
-rawPortfolio = pd.read_csv(filePath, skiprows=1)
-rawPortfolio.columns = ['Ticker',
-                        'Quantity',
-                        'Price',
-                        'DOA',
-                        'Action',
-                        'Sector']
-rawPortfolioArr = np.asarray(rawPortfolio)
-currentPortfolio = Portfolio(portfolioParse(rawPortfolioArr))
-aapl_stock = currentPortfolio.findStockByTicker("AAPL")
-print(aapl_stock)
-'''
